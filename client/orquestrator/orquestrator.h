f74db3c2767f6e3a1b9a511f2844112646d9ee0c
Javier Suarez <jsuarez@salesforce.com>
2022-08-07 00:44:25 +0200

    remove utils module and integrate it in orquestrator

--- client/orquestrator/orquestrator.h
+++ client/orquestrator/orquestrator.h
@@ -5,17 +5,15 @@
 #include "../sfdc_client/sfdc_client.h"
 #include "../resource_repo/resource_repo.h"
 #include "../resource_reader/resource_reader.h"
-#include "../utils/utils.h"
 
 class orquestrator
 {
     public:
         orquestrator(sfdc_client&& sfdc_client, resource_repo&& resource_repo, 
-                resource_reader&& resource_reader, utils&& utils):
+                resource_reader&& resource_reader):
             sfdc_client_(std::move(sfdc_client)),
             resource_repo_(std::move(resource_repo)),
-            resource_reader_(std::move(resource_reader)),
-            utils_(std::move(utils)) 
+            resource_reader_(std::move(resource_reader))
         {
             std::cout << "2 login details (endpoint): " << std::get<1>(resource_repo_.get_login_details()) << "\n";
             std::cout << "2 login details (token): " << std::get<0>(resource_repo_.get_login_details()) << "\n";
@@ -29,6 +27,7 @@ class orquestrator
         sfdc_client sfdc_client_;
         resource_repo resource_repo_;
         resource_reader resource_reader_;
-        utils utils_;
+
+        static std::string get_filename_from_filepath(const std::string& filepath);
 };
 #endif
